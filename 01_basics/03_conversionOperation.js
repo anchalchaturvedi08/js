e261f17582a870ec3ec8995582062f5e2470abcc
Anchal chaturvedi <129603987+anchalchaturvedi08@users.noreply.github.com>
2024-04-16 03:12:46 +0000

    basics01

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,72 @@
+let score = 33
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber);
+// console.log(ValueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = "ashi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ashi" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations ************************** //
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " ashi"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// agar string first hai to sbhi ko string me treat kiya jayega, aur agar string last me hai to phele jo conversion hai vo ho jayega
+
+// console.log((3 + 4) * (5 % 3));
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num2 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
---
+++ 01_basics/dataTypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bigNumber = 446653464746747775n
+
+
+
+// Reference (Non primitive)
+
+//  Array, Objects, Function
+
+const heros = ["Shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "ashi",
+    age: 13
+}
+
+const myfunction = function() {
+    console.log("Hello World");
+}
+
+console.log(typeof outsideTemp);
+
+//  link to study typeof Operator Results
+// https://262.ecma-international.org/5.1/#sec-11.4.3
