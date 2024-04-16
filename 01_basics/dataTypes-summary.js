4318d231bee6e3d0e6e3ee6e983bdba793d9c9b7
Anchal chaturvedi <129603987+anchalchaturvedi08@users.noreply.github.com>
2024-04-16 04:21:01 +0000

    basics01

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -32,7 +32,34 @@ const myfunction = function() {
     console.log("Hello World");
 }
 
-console.log(typeof outsideTemp);
+// console.log(typeof outsideTemp);
 
 //  link to study typeof Operator Results
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//  Stack (Primitive), Heap, (Non-Primitive)
+
+let myYoutubeName = "ashichaturvedidotcom"
+
+let anothername = myYoutubeName
+anothername = "techverse"
+
+console.log(anothername);
+console.log(myYoutubeName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "ashi@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
