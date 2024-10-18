da39adb2078d07c6d0fd5556fdda98a62e471de3
anchal chaturvedi <anchalchaturvedi08@gmail.com>
2024-10-18 21:45:07 +0530

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<= 1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+
+// JavaScript comparisons with null and undefined can be tricky. Let's see how they behave:
+
+
+// Comparing null with 0
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+// Comparing undefined with 0
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<0);
+
+// (===) In JavaScript, the === operator is known as the strict equality operator. It checks both the value and the data type of the operands to determine if they are equal.
+
+console.log("2"===2);
